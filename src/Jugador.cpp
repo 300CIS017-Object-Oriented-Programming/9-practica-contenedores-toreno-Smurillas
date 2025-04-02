686d7734ddc1e3aeb5fbed2feea9f397726dc3e8
SantiagoMurillas <smurillas9@icloud.com>
2025-04-02 10:52:49 -0500

    Agregado Datos Jugador.cpp

--- src/Jugador.cpp
+++ src/Jugador.cpp
@@ -1,2 +1,30 @@
+#include "Jugador.h"
+#include <iostream>
 
+Jugador::Jugador(string nick) {
+    nickname = nick;
+}
+
+void Jugador::inscribir(Videojuego* juego) {
+    for (auto v : videojuegos) {
+        if (v->codigo == juego->codigo) {
+            cout << "Ya estás inscrito en ese videojuego.\n";
+            return;
+        }
+    }
+    videojuegos.push_back(juego);
+    cout << "Inscripción realizada.\n";
+}
+
+void Jugador::mostrarVideojuegos() {
+    if (videojuegos.empty()) {
+        cout << "No estás inscrito en ningún videojuego.\n";
+        return;
+    }
+
+    cout << "Videojuegos de " << nickname << ":\n";
+    for (auto v : videojuegos) {
+        cout << "- " << v->nombre << "\n";
+    }
+}
 
